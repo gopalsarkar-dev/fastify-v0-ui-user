b5fd17bdbd71255530b5dfbff0627131fdc3950a
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-06-16 10:38:26 +0530

    Update Nave.tsx

--- src/components/Header/Nave.tsx
+++ src/components/Header/Nave.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import UserPost from "../UserPost";
 
 const Nave = () => {
@@ -5,7 +6,9 @@ const Nave = () => {
     <>
       <nav className="fixed top-0 w-full border-b backdrop-blur-sm">
         <section className="container mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
-          <div className="cursor-pointer text-xl font-bold">👤</div>
+          <Link href="/">
+            <div className="cursor-pointer text-xl font-bold">👤</div>
+          </Link>
           <div className="flex items-center justify-center gap-2.5">
             <div className="text-xl font-bold">☀️</div>
             <UserPost />
